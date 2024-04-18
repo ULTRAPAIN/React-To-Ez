fecb886972f1aac90c1244cd9e3cdb9776a0bc01
Dashrath Patel <dashrathpatel0810@gmail.com>
2024-04-18 12:25:51 +0530

    Designed with Taiwlind css

--- counter-hook/src/App.jsx
+++ counter-hook/src/App.jsx
@@ -1,28 +1,31 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   let counter =0
   const [count, setCount] = useState(counter)
 
 const addValue=()=>{
-if(count<20){
   setCount(count+1)
 }
-}
+
 const subtractValue=()=>{
-  if(count>0){
     setCount(count-1)
-  }
+}
+const resetValue=()=>{
+  setCount(0)
 }
   return (
     <>
-    <p>Click the Button Add , the counter increase up to 20 only not above that </p>
-    <p>Click the subtract button , the counter will decrease up to 0 only </p>
-      <h1>Counter Dynamic</h1>
-      <button onClick={addValue}>Add No</button>
-      <button onClick={subtractValue}>Subtract No</button>
-      <p>count is: {count}</p>
+    <div className="flex items-center justify-center h-screen bg-black">
+      <div className="relative flex flex-col items-center gap-10 text-3xl font-semibold text-center text-white">Increment & Decrement
+      <div className='flex justify-center gap-x-5 '>
+        <button className='text-6xl px-7 active:text-gray-600' onClick={subtractValue}>-</button>
+        <div className='text-6xl border-l border-r-2 border-gray-700 px-14'>{count}</div>
+        <button className='text-6xl px-7 active:text-gray-600' onClick={addValue}>+</button>  
+      </div>
+      <div className='w-1/2 px-2 py-2 font-semibold text-black duration-200 bg-gray-600 cursor-pointer rounded-xl hover:bg-gray-700 active:text-white' onClick={resetValue}>Reset</div>
+      </div>
+    </div>
     </>
   )
 }
